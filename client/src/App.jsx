908aab6624824dfa3fe1e3aaaf84aa2aab180ce7
RTIN <118107221+Romo3@users.noreply.github.com>
2024-01-25 01:17:53 -0500

    initialization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,29 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 import './global.css'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
-    <>
-      <h1>Blog Time</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+        
+        <Route path="/projects" element={<Projects />}/>
+        <Route path="/signin" element={<SignIn />}/>
+        <Route path="/signup" element={<SignUp />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navbar } from 'flowbite-react';
+
+const Header = () => {
+  return (
+    <Navbar className='border-b-2 px-2 py-2'>Header</Navbar>
+  )
+}
+
+export default Header
