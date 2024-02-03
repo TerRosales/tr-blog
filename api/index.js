302a165688e87716a6bffc1be9283c13e5b3e4fe
RTIN <118107221+Romo3@users.noreply.github.com>
2024-02-03 00:55:01 -0500

    create sign up api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 // use for hiding DB info
 import dotenv from 'dotenv'
 
@@ -18,10 +19,17 @@ mongoose
 // Server SetUp
 const app = express();
 
+app.use(express.json()) //this is telling Express to use the express.json() middleware for all incoming requests, enabling automatic parsing of JSON request bodies.
+
 app.listen(3000, () => {
     console.log('Server is running at Port: 3000')
 })
 
+// Routes Setup 
 
 app.use('/api/user', userRoutes
 );
+
+app.use('/api/auth', authRoutes
+);
+
