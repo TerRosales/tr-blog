f6eca122aad94d00f15c9653f9d2f71a20f3484a
RTIN <118107221+Romo3@users.noreply.github.com>
2024-02-08 07:17:20 -0500

    transfer to PC

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Label, TextInput } from 'flowbite-react'
 import { Link } from 'react-router-dom';
-import '../components/Header/Header.css';
 
 const SignUp = () => {
   return (
@@ -8,11 +8,12 @@ const SignUp = () => {
     <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
       {/* left */}
     <div className="">
-        <Link Link to='/' className="app__logo">
+        <Link Link to='/' className="text-[30px] font-bold dark:text-white">
           Pocket&nbsp;
-          <span className='style-span'>&#123;</span>&nbsp;<span className='logo__span'> Items: &infin;</span>&nbsp;
-          <span className='style-span'>&#125;</span>
+          <span className='text-teal-500 text-[30px]'>&#123;</span>&nbsp;<span className='px-2 py-1 text-[32px] rounded-lg text-white font-bold bg-gradient-to-b from-slate-900 to-teal-700 '> Items: &infin;</span>&nbsp;
+          <span className='text-teal-500 text-[30px]'>&#125;</span>
         </Link>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores optio deleniti possimus voluptatum? Ipsa itaque tempore ut quis, laboriosam, molestiae distinctio quae reiciendis voluptatibus praesentium alias, rerum aliquam.</p>
     </div>
     {/* right */}
     <div className=""></div>
