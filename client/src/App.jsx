852c75cfd29abc6486cc9916fa13af26828ff1cf
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2024-02-27 09:57:01 -0500

    Private Route for Dashboard Init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 
 import FooterComp from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -19,8 +20,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
