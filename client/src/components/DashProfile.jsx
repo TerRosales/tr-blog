1c9f4494bf134ee5312b3ee9e2a5c09d95099537
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2024-03-03 13:52:30 -0500

    Profile Pic Upload Init

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,18 +1,102 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { TextInput, Button } from "flowbite-react";
+import { FaCamera } from "react-icons/fa";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 const DashProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUplaodProgress, setImageFileUplaodProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const filePickerRef = useRef();
+  const [isHovered, setIsHovered] = useState(false);
+  console.log(imageFileUplaodProgress, imageFileUploadError);
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+  const uploadImage = async () => {
+    console.log("uploading image...");
+    //firebase storage rules
+    // service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**} {
+    //       allow read;
+    //       allow write: if request.request.size < 2 * 1024 * 1024 &&
+    //       request.resource.contentType.matches('image/.*')
+    //     }
+    //   }
+    // }
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUplaodProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError(
+          "Could not upload image (file must be less than 2MB)"
+        );
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+          setImageFileUrl(downloadUrl);
+        });
+      }
+    );
+  };
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl text-[white]">
         Profile
       </h1>
       <form className="flex flex-col gap-5">
-        <div className="w-32 h-32 self-center">
+        <div className="relative">
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            ref={filePickerRef}
+            hidden
+          />
+          {isHovered && (
+            <>
+              <span className="cursor-pointer absolute z-10 mt-16 inset-0 flex flex-col  items-center justify-center text-white font-semibold opacity-[0.7]">
+                Upload Profile Picture
+              </span>
+              <FaCamera className="cursor-pointer absolute mt-20 w-12 h-12 justify-center items-center text-white opacity-[0.7] left-1/2 -translate-x-1/2" />
+            </>
+          )}
+        </div>
+        <div
+          className="w-32 h-32 self-center cursor-pointer shadow-md everflow-hidden rounded-full hover:bg-white"
+          onClick={() => filePickerRef.current.click()}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
             className="rounded-full w-full h-full object-cover border-8 border-[#1f2939]"
           />
