5fba029f97497664cf0343aed3f6da2101b18be8
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2024-06-14 10:24:38 -0400

    about page in progress

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,14 +7,14 @@ const About = () => {
         <h3 className="my-7 text-center font-semibold text-3xl text-gray-500">
           About
         </h3>
-        <section className="text-white text-sm flex flex-col mx-auto text-center max-w-2xl">
+        <section className="text-white text-xl flex flex-col mx-auto text-center max-w-2xl">
           <img
             className="my-20 mx-auto w-[50%] h-[50%]"
             src={images.aboutImg}
             alt="about me image"
           />
           <section>
-            <p>
+            <p className="mb-20">
               Hi! I&apos;m a software developer based in the United States. I
               have a passion for creating and building Web applications that can
               provide value to anyone. I&apos;m always looking to learn new
